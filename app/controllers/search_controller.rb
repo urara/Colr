83fab6063af749f0f0b1191323de7eb4808d92cf
kaigi78 <kaigi@mbp.local>
2014-06-26 00:58:35 +0900

    fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < UIViewController
   def viewDidLoad
     super
 
-    # self.title = "Search"
-    # self.view.backgroundColor = UIColor.whiteColor
+    self.title = "Search"
+    self.view.backgroundColor = UIColor.whiteColor
 
     @text_field = UITextField.alloc.initWithFrame([[0,0], [160,26]])
     @text_field.placeholder = "#abcabc"
@@ -13,21 +13,12 @@ class SearchController < UIViewController
     @text_field.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 100)
     self.view.addSubview @text_field
 
-    @searcha = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    @searcha.setTitle("Search", forstate:UIControlStateNormal)
-    #@search.setTitle("Loading", forState:UIControlStateDisabled)
-    @searcha.accessibilityLabel = "Test mea"
-    @searcha.sizeToFit
-    # @search.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2 + 40)
-    @searcha.frame = CGRect.new([100,420], @searcha.frame.size) 
-    self.view.addSubview @searcha
-
-    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    @button.setTitle("Test me title", forState:UIControlStateNormal)
-    @button.accessibilityLabel = "Test me"
-    @button.sizeToFit
-    @button.frame = CGRect.new([100,210], @button.frame.size) 
-    self.view.addSubview @button
-
+    @search = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @search.setTitle("Search", forState:UIControlStateNormal)
+    @search.setTitle("Loading", forState:UIControlStateDisabled)
+    @search.sizeToFit
+    @search.center = CGPointMake(self.view.frame.size.width / 2, @text_field.center.y + 40)    
+    self.view.addSubview @search
+    
   end
 end
